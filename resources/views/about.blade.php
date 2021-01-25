3790556f0903fd73da3d84e83994db58d72dd80d
Antonia P <direiellie@gmail.com>
2021-01-25 19:27:25 +0100

    About US

---
+++ resources/views/about.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container mb-5">
+    <h1>About US</h1>
+
+    <h2>Our History</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+
+    <h2>Our Team</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+    c<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, dolorum est, amet, inventore sequi sapiente eveniet 
+    illo minus temporibus quasi doloribus soluta aspernatur adipisci explicabo. Illum officia tempora neque aperiam?</p>
+</div>
+@endsection
