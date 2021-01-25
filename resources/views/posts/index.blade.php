cdd8f4f3222fc69e9f8556ca9a203f2d953a090c
Antonia P <direiellie@gmail.com>
2021-01-25 22:38:45 +0100

    Implementazione Forelse

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,20 @@
 @section('content')
 <div class="container mb-5">
     <h1>BLOG ARCHIVE</h1>
+
+    @forelse ($posts as $post)
+    <article class="mb-5">
+        <h2>{{ $post->title }}</h2>
+        <h5>{{ $post->created_at->format('d/m/Y') }}</h5>
+
+        <p>{{ $posts->body }}</p>
+        <a href="#">Read More</a>
+
+    </article>
+        
+    @empty
+        <p>No post found. Go and <a href="#">create a new one.</a></p>
+    @endforelse
     
     
 </div>
